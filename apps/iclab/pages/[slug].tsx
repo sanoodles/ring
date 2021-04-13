ee7161fddd7369c31d257abfab6f9e572a812f55
Lorenzo García Moreno <dev@lorenzogm.com>
2021-04-13 10:11:41 +0200

    feat(pages): add dynamic content pages

---
+++ apps/iclab/pages/[slug].tsx
@@ -0,0 +1,91 @@
+import React, { ReactElement } from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Error from 'next/error'
+
+import { ContentTemplate } from '@ring/iclab/ui'
+import {
+  IContentPage,
+  IConfig,
+  IContentPageFields,
+} from '../types/generated/contentful'
+import contentful from '../services/contentful'
+import selectNavigationItems from '../services/contentful/selectNavigationItems'
+
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  const preview = false
+  const contentPageEntriesPerLocale = await Promise.all(
+    locales.map((locale) =>
+      contentful(preview).getEntries<IContentPageFields>({
+        content_type: 'contentPage',
+        'fields.slug[ne]': 'home',
+        locale,
+      }),
+    ),
+  )
+
+  const paths = []
+
+  contentPageEntriesPerLocale.forEach((contentPageEntries) => {
+    contentPageEntries.items.forEach((item) => {
+      paths.push(`/${item.fields.slug}`)
+    })
+  })
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  locale,
+  preview = false,
+}) => {
+  const [configEntries, contentPageEntries] = await Promise.all([
+    contentful(preview).getEntries<IContentPage>({
+      content_type: 'config',
+      locale,
+    }),
+    contentful(preview).getEntries<IContentPage>({
+      content_type: 'contentPage',
+      'fields.slug': params?.slug,
+      locale,
+    }),
+  ])
+
+  const [config] = configEntries.items
+  const [contentPage] = contentPageEntries.items
+
+  if (!contentPage) {
+    return {
+      props: {},
+    }
+  }
+
+  return {
+    props: { config, contentPage },
+  }
+}
+
+export default function ContentPage({
+  config,
+  contentPage,
+}: ContentPageProps): ReactElement {
+  if (!config || !contentPage) {
+    return <Error statusCode={404} />
+  }
+
+  const navigationItems = selectNavigationItems(config)
+  return (
+    <ContentTemplate
+      navigationItems={navigationItems}
+      fields={contentPage.fields}
+    />
+  )
+}
+
+type ContentPageProps = {
+  config: IConfig
+  contentPage: IContentPage
+}
